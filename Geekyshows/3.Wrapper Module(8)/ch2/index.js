6a0f6e6e619d0166d4a3e17eab1a2d44af0b581b
satyam-seth <satyam1998.1998@gmail.com>
2022-04-03 16:37:08 +0530

    4th commit

---
+++ Geekyshows/3.Wrapper Module(8)/ch2/index.js
@@ -0,0 +1,9 @@
+// // Module Wrapper
+// (function (exports, require, module, __filename, __dirname) {
+//   // Module code actually live in here
+// });
+
+console.log("Hello GeekyShows");
+console.log(__dirname);
+console.log(__filename);
+console.log(module);
